024e7142aea4d79dc7c970b664644b510d83af56
Jeremías <jeremiias122002@gmail.com>
2023-09-10 12:48:12 +0200

    updates

--- crud_mern_stack/src/App.jsx
+++ crud_mern_stack/src/App.jsx
@@ -1,4 +1,5 @@
-// import { useState } from 'react'
+import React from 'react'
+import axios from 'axios'
 import './App.css'
 
 import AgregarUsuario from './AgregarUsuario'
@@ -7,8 +8,43 @@ import ListaUsuario from './ListaUsuario'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+// const baseURL2 = "https://jsonplaceholder.typicode.com/posts";
+
 function App() {
-  // const [count, setCount] = useState(0)
+  
+  // método get
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null; 
+
+  // método post
+  /* const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL2}/1`).then(response => {
+      setPost(response.data);
+    });
+  }, []);
+
+  function createPost() {
+    axios
+      .post(baseURL2, {
+        title: "Hi world !",
+        body: "Using post with axios in React."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+
+      if (!post) return "No post !";
+  } */
 
   return (
     <>
@@ -18,7 +54,7 @@ function App() {
           <div className="container">
             <a className="navbar-brand" href="#">CRUD MERN Stack - Jere</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -31,9 +67,9 @@ function App() {
         </ul>
       </div>
     </div>
-  </nav>
+        </nav>
 
-  <h1>CRUD MERN STACK - WELCOME !</h1>
+  <h1>WELCOME !</h1>
 
         <BrowserRouter>
         
@@ -45,11 +81,25 @@ function App() {
 
         </BrowserRouter>
 
-      </div>
-      <p className="read-the-docs">
-        Practising with React due to it is so much used it.
-      </p>
 
+        <div className="container-fluid mt-4 mb-4 p-2 bg-body-secondary">
+          <code>Método GET con Axios</code>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+
+        <div className="container-fluid mt-4 mb-4 p-2 bg-body-secondary">
+          <code>Método POST con Axios</code>
+          <h3>{}</h3>
+          <p>{}</p>
+
+          <button className="btn btn-primary">Crear Post</button>
+        </div>
+
+        <p className="read-the-docs">
+          Practising with React due to it is so much used it.
+        </p>
+      </div>
     </>
   )
 }
